68643a0f5d841c26bb7497ee35a872dbca100dfc
Desper-jpg <mainaeliud2392@gmail.com>
2025-12-19 12:16:06 +0300

    Update 42k-shoe-shop-app.js

--- 42k-shoe-shop-app.js
+++ 42k-shoe-shop-app.js
@@ -5,7 +5,7 @@ const products = [
     { id: 3, name: "air force 1", price: 20 * 130, image: "assets/air-force-3.jpeg" },
     { id: 4, name: "Air Max 97", price: 27.6923076923 * 130, image: "assets/Air-Max-97.jpg"},
     { id: 5, name: "New Balance", price: 26.1538461538 * 130, image: "assets/new-balance.jpg" },
-    { id: 6, name: "Naked wolfe", price: 23.07692307692307 * 130, image: "assets/naked-wolfe.jpg" },
+    { id: 6, name: "Nike Shox", price: 33.0769230769 * 130, image: "assets/New-Shox.jpeg"},
     { id: 7, name: "Vapor Max", price: 27.6923076923 * 130, image: "assets/vapor-max.jpg" },
     { id: 8, name: "jordan", price: 19.22307692307692 * 130, image: "assets/jordan.jpeg" },
     { id: 9, name: "Blade max sport", price: 21.53846153846153 * 130, image: "assets/blade-max-sport.jpg" },
@@ -84,7 +84,8 @@ const products = [
     { id: 82, name: "Timberland", price: 32.3076923077 * 130, image: "assets/Timberland.jpg"},
     { id: 83, name: "Black & White Air Force 1", price: 26.1538461538 * 130, image: "assets/Black-&-White-Air-Force-1.jpg"},
     { id: 84, name: "adidas ultraboost", price: 27.69230769230769 * 130, image: "assets/adidas-ultraboost.jpg"},
-    { id: 85, name: "vans", price: 17.6923076923 * 130, image: "assets/vans.jpg"}
+    { id: 85, name: "vans", price: 17.6923076923 * 130, image: "assets/vans.jpg"},
+    { id: 86, name: "Naked wolfe", price: 23.07692307692307 * 130, image: "assets/naked-wolfe.jpg" }
     
 ];
 
@@ -349,6 +350,7 @@ function searchProducts() {
   currentPage = 1;
   renderPaginatedProducts();
 }
+
 
 
 
