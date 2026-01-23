e177a62b96a704f541d4249955c8417edab2fc36
Desper-jpg <mainaeliud2392@gmail.com>
2026-01-23 10:11:32 +0300

    Update 42k-shoe-shop-app.js

--- 42k-shoe-shop-app.js
+++ 42k-shoe-shop-app.js
@@ -31,7 +31,7 @@ const products = [
     { id: 29, name: "Black & Green Vans", price: 15.3846153846 * 130, image: "assets/black-&-white-vans.jpg"},
     { id: 30, name: "Black Men Leather Shoe", price: 21.5384615385 * 130, image: "assets/black-men-leather-shoe.jpg"},
     { id: 31, name: "D&G Platform Lowtop", price: 20 * 130, image: "assets/d&g-platform-low-top.jpg"},
-    { id: 32, name: "Men's Leather Sandal (Out Of Stock )", price: 13.06923076923076 * 130, image: "assets/men's-leather-sandal.jpg"},
+    { id: 32, name: "New Balance 9060", price: 27.6923076923 * 130, image: "assets/New-Balance-9060.jpg"},
     { id: 33, name: "Men's Leather Shoe", price: 26.1538461538 * 130, image: "assets/men's-leather-shoe.jpg"},
     { id: 34, name: "New Balance 990v3", price: 20.7692307692 * 130, image: "assets/new-balance-990v3.jpg"},
     { id: 35, name: "Air Force 1", price: 23.0769230769 * 130, image: "assets/nike-air-force.jpg"},
@@ -350,6 +350,7 @@ function searchProducts() {
   currentPage = 1;
   renderPaginatedProducts();
 }
+
 
 
 
